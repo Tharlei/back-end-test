afb48e2924714d2ce2e93c7556200523eb3149b1
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2020-09-23 15:10:24 -0300

    fixing product search

--- app/Http/Controllers/ProductSearchController.php
+++ app/Http/Controllers/ProductSearchController.php
@@ -15,16 +15,14 @@ class ProductSearchController extends Controller
      */
     public function show(Request $request)
     {
-        $id = $request->id ?? 0;
-        $name = $request->name ?? '';
         $products = Product::select('id', 'name', 'sku', 'price');
 
         if (!empty($request->id)) {
-            $products = $products->where('id', $id);
+            $products = $products->where('id', $request->id);
         }
 
         if (!empty($request->name)) {
-            $products = $products->orWhere('name', 'like', "%{$request->name}%");
+            $products = $products->orWhere('name', 'like', "%{$$request->name}%");
         }
 
         $products = $products->get();
